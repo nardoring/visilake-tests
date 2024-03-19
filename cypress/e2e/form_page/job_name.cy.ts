ad3aa7de3387533e083047781c194f235f6e562c
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-03-19 11:13:39 -0600

    Add tests for job name field

---
+++ cypress/e2e/form_page/job_name.cy.ts
@@ -0,0 +1,24 @@
+import {
+  job_name_selector,
+  submit_job_button_selector,
+} from "../selectors/form";
+
+describe("Form Page: Job Name Entry Validation", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:45139/");
+  });
+
+  it("error for no job name entry", () => {
+    cy.get(submit_job_button_selector).click();
+
+    cy.get(job_name_selector).should("have.class", "ring-1 ring-red");
+  });
+
+  it("passes for valid job name entry", () => {
+    cy.get(job_name_selector).type("Form Page: Job Name Entry Validation");
+
+    cy.get(submit_job_button_selector).click();
+
+    cy.get(job_name_selector).should("not.have.class", "ring-1 ring-red");
+  });
+});
