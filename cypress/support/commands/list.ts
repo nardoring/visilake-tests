89a9006b8feeb19344906d9fcb60b156acaf59e1
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-03-21 21:51:10 -0600

    Add command to show all columns in table

--- cypress/support/commands/list.ts
+++ cypress/support/commands/list.ts
@@ -1,9 +1,23 @@
 declare namespace Cypress {
   interface Chainable<Subject = any> {
     clickColumnsButton(): Chainable<Subject>;
+    showAllColumns(): Chainable<Subject>;
   }
 }
 
 Cypress.Commands.add("clickColumnsButton", () => {
   cy.get(".ag-side-buttons").contains("Columns").click();
+});
+
+Cypress.Commands.add("showAllColumns", () => {
+  cy.clickColumnsButton();
+  cy.get(".ag-virtual-list-item").each(($item) => {
+    cy.wrap($item).within(() => {
+      const checkbox = $item.find(".ag-checkbox-input");
+      const isChecked = checkbox.prop("checked");
+      if (!isChecked) {
+        cy.wrap(checkbox).click();
+      }
+    });
+  });
 });
