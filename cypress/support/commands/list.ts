9fc492820b17d13dadb26d6a68216aff29520efd
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-03-28 11:13:04 -0600

    Fix date range assertions and fixture format

--- cypress/support/commands/list.ts
+++ cypress/support/commands/list.ts
@@ -44,9 +44,18 @@ Cypress.Commands.add("verifyTableRowData", { prevSubject: true }, ($row, rowData
     cy.get('[col-id="sources"]').should("contain.text", sources.join(", "));
     cy.get('[col-id="analysisTypes"]').should("contain.text", analysis_types.join(", "));
     cy.get('[col-id="granularity"]').should("contain.text", granularity);
-    // cy.get('[col-id="dateRange"]').should("contain.text", date_range.join(" - ")); //TODO: Fix Date Range formatting & col-id
+    cy.get('[col-id="dateRange"]').should("contain.text", formatDateRange(date_range).join(" - "));
     cy.get('[col-id="date"]').should("contain.text", date);
     cy.get('[col-id="author"]').should("contain.text", author);
     cy.get('[col-id="jobStatus"]').should("contain.text", status);
   });
 });
+
+const formatDateRange = (dates: string[]): string[] => {
+  return dates.map(date => {
+      const [datePart, timePart] = date.split(' ');
+      const [month, day, year] = datePart.split('-');
+      const formattedDate = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+      return `${formattedDate} ${timePart}`;
+  });
+};
